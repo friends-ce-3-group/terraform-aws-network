fb5fdfeb162bc341d021c47869beb99814420a61
Low Chee Meng <lowcheemeng02@gmail.com>
2023-10-25 23:55:37 +0800

    add output exports

---
+++ outputs.tf
@@ -0,0 +1,28 @@
+output "vpc_id" {
+    value = aws_vpc.vpc.id
+}
+
+output "subnets_private" {
+    value = aws_subnet.subnets_private
+}
+
+output "subnets_public" {
+    value = aws_subnet.subnets_public
+}
+
+output "route_tables_private" {
+    value = aws_route_table.rt_private
+}
+
+output "route_tables_private" {
+    value = aws_route_table.rt_private
+}
+
+output "internet_gateway" {
+    value = aws_internet_gateway.internet_gw
+}
+
+output "nat_gateways" {
+    value = aws_nat_gateway.pvt_subnet_nats
+}
+
